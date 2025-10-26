95c610f5dd0d53914b8ce7783c361ae03f5ab465
Juan Martin Feliu <jmfeliu04@gmail.com>
2025-10-26 17:02:18 -0300

    Tercer commit, TP terminado (faltaria ordenar el codigo y comentarlo para mayor comprension).

--- Controllers/PresupuestoController.cs
+++ Controllers/PresupuestoController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Presupuestos;
+using presupuestosRepository;
 
 namespace tl2_tp7_2025_JuanMartinFeliu.Controllers;
 
@@ -6,5 +8,49 @@ namespace tl2_tp7_2025_JuanMartinFeliu.Controllers;
 [Route("[controller]")]
 public class PresupuestoController : ControllerBase
 {
-    
+    private PresupuestoRepository presupuestoRepository;
+    public PresupuestoController()
+    {
+        presupuestoRepository = new PresupuestoRepository();
+    }
+
+    /*
+    ● POST /api/Presupuesto: Permite crear un Presupuesto.
+    ● POST /api/Presupuesto/{id}/ProductoDetalle: Permite agregar un Producto existente
+    y una cantidad al presupuesto.
+    ● GET /api/Presupuesto/{id}: Obtener detalles de un Presupuesto por su ID.
+    ● GET /api/presupuesto: Permite listar los presupuestos existentes.
+    ● DETELE /api/Presupuesto/{id}: Permite eliminar un Presupuesto.
+    */
+
+    [HttpPost("CrearPresupuesto")]
+    public ActionResult<string> CrearPresupuesto(Presupuesto presup)
+    {
+        presupuestoRepository.CrearPresupuesto(presup);
+        return Ok("Presupuesto creado exitosamente");
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Presupuesto> obtenerPresupuesto(int idBuscado)
+    {
+        Presupuesto presu;
+        presu = presupuestoRepository.ObtenerDetallesPresupuesto(idBuscado);
+        return (presu);
+    }
+
+    [HttpGet("ListarPresupuestos")]
+    public ActionResult<List<Presupuesto>> listarPresupuesto()
+    {
+        List<Presupuesto> listita;
+        listita = presupuestoRepository.ListarPresupuestos();
+        return (listita);
+
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult<string> eliminarPresupuesto(int idBuscado)
+    {
+        presupuestoRepository.EliminarPresupuesto(idBuscado);
+        return Ok("Presupuesto eliminado exitosamente");
+    }
 }
--- Repositorios/PresupuestosRepository.cs
+++ Repositorios/PresupuestosRepository.cs
@@ -3,95 +3,99 @@ using Microsoft.Data.Sqlite;
 using Presupuestos;
 using PresupuestosDetalle;
 
-public interface IPresupuestoRepository
+namespace presupuestosRepository
 {
-    public void CrearPresupuesto(Presupuesto presup);
-    public List<Presupuesto> ListarPresupuestos();
-    public Presupuesto ObtenerDetallesPresupuesto(int idBuscado);
-    public void AgregarProducto(int idBuscado);
-    public void EliminarPresupuesto(int idBuscado);
 
-}
-
-public class PresupuestoRepository : IPresupuestoRepository
-{
-    private string connectionString = "Data Source=Data/Tienda.db";
-    public void CrearPresupuesto(Presupuesto presup)
+    public interface IPresupuestoRepository
     {
-        using var conexion = new SqliteConnection(connectionString);
-        conexion.Open();
-        string sql = "INSERT INTO Presupuestos (idPresupuesto, NombreDestinatario, FechaCreacion) VALUES (@idPresupuesto, @NombreDestinatario, @FechaCreacion)";
-        using var comando = new SqliteCommand(sql, conexion);
-        comando.Parameters.Add(new SqliteParameter("@idPresupuesto", presup.IdPresupuesto));
-        comando.Parameters.Add(new SqliteParameter("@NombreDestinatario", presup.NombreDestinatario));
-        comando.Parameters.Add(new SqliteParameter("@FechaCreacion", presup.FechaCreacion1)); comando.ExecuteNonQuery();
+        public void CrearPresupuesto(Presupuesto presup);
+        public List<Presupuesto> ListarPresupuestos();
+        public Presupuesto ObtenerDetallesPresupuesto(int idBuscado);
+        public void AgregarProducto(int idBuscado);
+        public void EliminarPresupuesto(int idBuscado);
+
     }
 
-    public List<Presupuesto> ListarPresupuestos()
+    public class PresupuestoRepository : IPresupuestoRepository
     {
-        var presupuesto = new List<Presupuesto>();
-        using var conexion = new SqliteConnection(connectionString);
-        conexion.Open();
-
-        string sql = "SELECT idPresupuesto, NombreDestinatario, FechaCreacion FROM Presupuestos";
-        using var comando = new SqliteCommand(sql, conexion);
-        using var lector = comando.ExecuteReader();
-
-        while (lector.Read())
+        private string connectionString = "Data Source=Data/Tienda.db";
+        public void CrearPresupuesto(Presupuesto presup)
         {
-            var pre = new Presupuesto
-            {
-                IdPresupuesto = Convert.ToInt32(lector["idPresupuesto"]),
-                NombreDestinatario = lector["NombreDestinatario"].ToString(),
-                FechaCreacion1 = Convert.ToDateTime(lector["FechaCreacion"])
-            };
-            presupuesto.Add(pre);
+            using var conexion = new SqliteConnection(connectionString);
+            conexion.Open();
+            string sql = "INSERT INTO Presupuestos (idPresupuesto, NombreDestinatario, FechaCreacion) VALUES (@idPresupuesto, @NombreDestinatario, @FechaCreacion)";
+            using var comando = new SqliteCommand(sql, conexion);
+            comando.Parameters.Add(new SqliteParameter("@idPresupuesto", presup.IdPresupuesto));
+            comando.Parameters.Add(new SqliteParameter("@NombreDestinatario", presup.NombreDestinatario));
+            comando.Parameters.Add(new SqliteParameter("@FechaCreacion", presup.FechaCreacion1)); comando.ExecuteNonQuery();
         }
 
-        return presupuesto;
-    }
-
-    public Presupuesto ObtenerDetallesPresupuesto(int idBuscado)
-    {
-        using var connection = new SqliteConnection(connectionString);
-        connection.Open();
-
-        // WHERE sobre la columna correcta
-        string consulta = "SELECT  idPresupuesto, NombreDestinatario, FechaCreacion FROM Presupuesto WHERE idPresupuesto = @Id";
-        using var comando = new SqliteCommand(consulta, connection);
-        comando.Parameters.AddWithValue("@Id", idBuscado);
-
-        using var lector = comando.ExecuteReader();
-        if (lector.Read())
+        public List<Presupuesto> ListarPresupuestos()
         {
-            var pre = new Presupuesto
+            var presupuesto = new List<Presupuesto>();
+            using var conexion = new SqliteConnection(connectionString);
+            conexion.Open();
+
+            string sql = "SELECT idPresupuesto, NombreDestinatario, FechaCreacion FROM Presupuestos";
+            using var comando = new SqliteCommand(sql, conexion);
+            using var lector = comando.ExecuteReader();
+
+            while (lector.Read())
             {
-                IdPresupuesto = Convert.ToInt32(lector["idPresupuesto"]),
-                NombreDestinatario = lector["NombreDestinatario"].ToString(),
-                FechaCreacion1 = Convert.ToDateTime(lector["FechaCreacion"])
-            };
-            return pre;
+                var pre = new Presupuesto
+                {
+                    IdPresupuesto = Convert.ToInt32(lector["idPresupuesto"]),
+                    NombreDestinatario = lector["NombreDestinatario"].ToString(),
+                    FechaCreacion1 = Convert.ToDateTime(lector["FechaCreacion"])
+                };
+                presupuesto.Add(pre);
+            }
+
+            return presupuesto;
         }
 
-        return null;
+        public Presupuesto ObtenerDetallesPresupuesto(int idBuscado)
+        {
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            // WHERE sobre la columna correcta
+            string consulta = "SELECT  idPresupuesto, NombreDestinatario, FechaCreacion FROM Presupuesto WHERE idPresupuesto = @Id";
+            using var comando = new SqliteCommand(consulta, connection);
+            comando.Parameters.AddWithValue("@Id", idBuscado);
+
+            using var lector = comando.ExecuteReader();
+            if (lector.Read())
+            {
+                var pre = new Presupuesto
+                {
+                    IdPresupuesto = Convert.ToInt32(lector["idPresupuesto"]),
+                    NombreDestinatario = lector["NombreDestinatario"].ToString(),
+                    FechaCreacion1 = Convert.ToDateTime(lector["FechaCreacion"])
+                };
+                return pre;
+            }
+
+            return null;
+        }
+
+        public void AgregarProducto(int idBuscado)
+        {
+
+        }
+
+        public void EliminarPresupuesto(int idBuscado)
+        {
+            using var conexion = new SqliteConnection(connectionString);
+            conexion.Open();
+
+            string sql = "DELETE FROM Presupuestos WHERE idPresupuesto = @Id";
+            using var comando = new SqliteCommand(sql, conexion);
+            comando.Parameters.AddWithValue("@Id", idBuscado);
+
+            comando.ExecuteNonQuery();
+        }
+
+
     }
-
-    public void AgregarProducto(int idBuscado)
-    {
-
-    }
-
-    public void EliminarPresupuesto(int idBuscado)
-    {
-        using var conexion = new SqliteConnection(connectionString);
-        conexion.Open();
-
-        string sql = "DELETE FROM Presupuestos WHERE idPresupuesto = @Id";
-        using var comando = new SqliteCommand(sql, conexion);
-        comando.Parameters.AddWithValue("@Id", idBuscado);
-
-        comando.ExecuteNonQuery();
-    }
-    
-
 }
